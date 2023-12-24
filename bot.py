070f589d80533aa3479572f3b82fe50e0b76cb72
root <lpr.khb@gmail.com>
2023-12-24 07:34:55 +0000

    Init

---
+++ bot.py
@@ -0,0 +1,20 @@
+﻿import telebot
+
+
+Token = ''
+
+
+@bot.message_handler(commands=['start'])
+def start_page(message):
+    pass
+
+
+@bot.message_handler(func=lambda message: True)
+def send_message(message):
+    bot.send_message(message.chat.id, "")
+    return
+
+
+if __name__ = __main__:
+   bot = telebot.TeleBot(f"{Token}")
+   bot.polling(none_stop=True)
